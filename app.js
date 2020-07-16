d0beea5643c0b34c8efd74d8ef195e9f88fbf07c
Felipe Ospina <felipe.ospina@MINEROS.COM.CO>
2020-07-16 17:23:33 -0500

    Create process and stage buttons functionality.

--- app.js
+++ app.js
@@ -27,4 +27,26 @@ class Process {
 
 const p1 = new Process('contrato whateva', 'felipe');
 
-console.log(p1.createStage('firmas', '2020 / 07 / 20'));
+const createProcessBtn = document.getElementById('create-process-btn');
+const section = document.querySelector('.open-processes');
+const wrapper = document.querySelector('.wrapper');
+const closeFormBtn = document.getElementById('close-form-btn');
+const addStageBtn = document.getElementById('add-stage-btn');
+const newStage = document.querySelector('#new-stage');
+
+if (createProcessBtn) {
+	createProcessBtn.addEventListener('click', () => {
+		wrapper.classList.add('show');
+	});
+}
+
+if (addStageBtn) {
+	addStageBtn.addEventListener('click', () => {
+		newStage.classList.add('show');
+		console.log('prueba');
+	});
+}
+
+closeFormBtn.addEventListener('click', () => {
+	wrapper.classList.remove('show');
+});
